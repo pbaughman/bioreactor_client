5c90981d2cc9538574aa926ba4d024d27c7564c2
Peter Baughman <4884343+pbaughman@users.noreply.github.com>
2021-09-18 01:03:03 +0100

    Initial commit - rough out python package

---
+++ README.md
@@ -0,0 +1,10 @@
+# Resilliance Bioreactor Client
+
+This python package provides a command-line client to Resilliance's bioreactor simulator API.
+
+Example usage:
+
+```
+$ python3 -m pip install <path_to_bioreactor_client>
+$ bioreactor_batch
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="bioreactor_client",
+    version="1.0",
+    desciption="A code exercise to build a client for a simulated bioreactor API",
+    author="Peter Baughman",
+    packages=["bioreactor_client"],
+    install_requires=[
+        "requests",
+    ],
+    tests_require=[
+        "pytest"
+    ],
+    entry_points={
+        "console_scripts": ["run_reactor=bioreactor_client.cmd:main"]
+    },
+)
